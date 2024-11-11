e5a53eb6eeee7d1fd4cc0c917ebe51353cd2a1a2
Avinash  Dhuriya <160625030+avinash-dhuriyaa@users.noreply.github.com>
2024-11-11 15:13:47 +0530

    Update practice_2.js

--- practice_2.js
+++ practice_2.js
@@ -2,7 +2,6 @@
 // of 10% OFF on them. Change the array to store final price after applying offer
 
 // 1st Method
-
 let arry = [250, 645, 300, 900, 50]
 for(let i =0; i<arr.length;i++){
     arr[i] = arr[i] - (arr[i]*0.1)
