8cc0ecc74fc21e0c79d5932b935c217561299215
Avinash <anjalirajesh1412089@gmail.com>
2024-11-11 15:26:06 +0530

    "Added Arry_method.js with examples of array methods (push, pop, shift, unshift, length) and removed unshift.js"

---
+++ Arry_method.js
@@ -0,0 +1,25 @@
+// 1.push()-Ye method array ke end mein ek item add karta hai.
+let arr = [1,2,3,4,5,6]
+arr.push(7)
+console.log(arr) // output = [1,2,3,4,5,6,7]
+
+// 2.pop()-Ye method array ke end se ek item remove karta hai.
+let arr1 = [1,2,3,4,5,6]
+arr1.pop()
+console.log(arr1) // output = [1,2,3,4,5]
+
+// 3.shift() – Ye method array ke pehle element ko remove karta hai.
+let arr2 = [1,2,3,4,5,6]
+arr2.shift()
+console.log(arr2) // output = [2,3,4,5,6]
+
+// 4.unshift() – Ye method array ke beginning mein ek item add karta hai.
+let arr4 = [1,2,3,4,5]
+arr4.unshift(0)
+console.log(arr4) // output = [0,1,2,3,4,5]
+//5 length – Ye property array mein total elements ki ginti batati hai.
+let arr5 = [1,2,3,4,5]
+console.log(arr5.length) // output = 5
+
+
+
--- unshift.js
+++
@@ -1,6 +0,0 @@
-let arr =[2,3,4,,5,6]
-arr.unshift(0,1)
-console.log(arr)
-for (const item of object) {
-    
-}
