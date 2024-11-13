e773ade397d142d8ca2120bcc0bd980da35f1023
Avinash  Dhuriya <160625030+avinash-dhuriyaa@users.noreply.github.com>
2024-11-13 16:33:47 +0530

    Update practice_2.js

--- practice_2.js
+++ practice_2.js
@@ -1,7 +1,7 @@
  //For a given array with prices of 5 items ->[250, 645, 300, 900, 50] All items have an offer 
 // of 10% OFF on them. Change the array to store final price after applying offer
 
-// 1st Method
+ // 1st Method
 let arry = [250, 645, 300, 900, 50]
 for(let i =0; i<arr.length;i++){
     arr[i] = arr[i] - (arr[i]*0.1)
@@ -10,13 +10,13 @@ for(let i =0; i<arr.length;i++){
     console.log(arr)
 
 
-// 2nd method
+ // 2nd method
 let arr = [250, 645, 300, 900, 50]
 let temp =[]
 const mult=0.9;
-// for(let i =0;i<arr.length; i++){
-//     temp[i] = arr[i]*mult;
-// }
+ // for(let i =0;i<arr.length; i++){
+ //     temp[i] = arr[i]*mult;
+ // }
 for(let val of arr){
     temp.push(val*mult)
 }
