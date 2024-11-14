5f26f55e2b6151a32300f5fc283fb8f984974dac
Avinash  Dhuriya <160625030+avinash-dhuriyaa@users.noreply.github.com>
2024-11-14 15:05:27 +0530

    Update arry.prac.js

--- arry.prac.js
+++ arry.prac.js
@@ -1,3 +1,9 @@
+// Qus1.Remove Elements from a JavaScript Array
 let arr = [1,22,33,44,55,66,77]
   arr.pop();
-console.log(arr)
+console.log(arr) // output [ 1, 22, 33, 44, 55, 66 ]
+
+// Qus2.How to Remove Duplicate Elements from JavaScript Array
+let arr1 = [1,2,2,3,4,4,5,6]
+let arry=[...new Set(arr1)];
+console.log(arry) // output [ 1, 2, 3, 4, 5, 6 ]
