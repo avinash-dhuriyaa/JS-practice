c34eb28750983297aaa79752315e02cf8627c05d
Avinash <anjalirajesh1412089@gmail.com>
2024-12-15 17:58:08 +0530

    update files

--- Dom_pra01/index.js
+++ Dom_pra01/index.js
@@ -1,3 +1,5 @@
+//Qus.1 Create a page with two images and a button. When the button is clicked, swap the source attribute of the images.
+
 // let btn = document.querySelector("button");
 // let pic1 = document.querySelector(".pic1")
 // let pic2 = document.querySelector(".pic2")
@@ -8,6 +10,9 @@
 // btn.addEventListener('click', function(){
 //     pic2.setAttribute('src',"pic1.jpg")
 //     })
+
+//Qus.2 Create a form with input fields and a submit button. Use JavaScript to validate the form and display an error message if 
+// the input is invalid.
 
 // let form = document.querySelector("form");
 // let np1= document.querySelector('#np1');
@@ -21,15 +26,20 @@
 //         h4.style.color = "red" ;  
 //     }
 // })
-document.getElementById('green').onclick = function(){
-    document.body.style.backgroundColor = 'green'
-    }
-    document.getElementById('red').onclick = function(){
-        document.body.style.backgroundColor = 'red'
-        }
-        document.getElementById('blue').onclick = function(){
-            document.body.style.backgroundColor = 'blue'
-            }
+// Qus.3 Create a three button  click button change backgroundColor 
+
+// document.getElementById('green').onclick = function(){
+//     document.body.style.backgroundColor = 'green'
+//     }
+//     document.getElementById('red').onclick = function(){
+//         document.body.style.backgroundColor = 'red'
+//         }
+//         document.getElementById('blue').onclick = function(){
+//             document.body.style.backgroundColor = 'blue'
+//             }
+
+//Qus.4 Create an unordered list. Allow users to add and remove list items dynamically using buttons.
+
 
 
 
