1d71402e65b17b32da0c4b9cb0cf53faf12f4edd
Avinash <anjalirajesh1412089@gmail.com>
2025-02-22 22:50:31 +0530

    add file

---
+++ object.js
@@ -0,0 +1,28 @@
+// let user ={
+//     name: "John",
+//     age: 30,
+//     occupation: "Software Engineer",
+//     hobbies: ["reading", "hiking", "coding"],
+//     location: "Allahabad"
+// }
+// console.log( user.age)
+
+
+// let user ={}
+// user.name = "John"
+// user.age = 30
+// user.occupation = "Software Engineer"
+// user.hobbies = ["reading", "hiking", "coding"]
+// user.location = "Allahabad"
+// let user2 ={
+//    email: "avi@hmail.com",
+//    fullname : { 
+//     userfullname :{
+//         firstname : "tony",
+//         lastname : "Doe",
+//     }
+
+//    }
+// }
+// console.log(user2.fullname.userfullname.firstname)
+     
