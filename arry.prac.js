4a9ff0993a57f35ed0797b309a0670e6f0155195
Avinash <anjalirajesh1412089@gmail.com>
2025-04-11 17:55:16 +0530

    add files

--- arry.prac.js
+++ arry.prac.js
@@ -6,5 +6,6 @@ console.log(arr) // output [ 1, 22, 33, 44, 55, 66 ]
 // Qus2.How to Remove Duplicate Elements from JavaScript Array
 let arr1 = [1,2,2,3,4,4,5,6]
 let arry=[...new Set(arr1)];
+
 console.log(arry) // output [ 1, 2, 3, 4, 5, 6 ]
 
